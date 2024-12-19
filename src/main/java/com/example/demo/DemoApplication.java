61ad20f5d43fa5f62895798bcc16edb249ea0dfd
Goga-Rid <wopa22@list.ru>
2024-12-19 16:05:38 +0300

    [FEATURE] create database and model

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
 public class DemoApplication {
+
+	@Autowired
+	private SessionFactory sessionFactory;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
+
+
+	@GetMapping("/users")
+
+
+
+	@GetMapping("/user")
+
 
 }
